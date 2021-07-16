bdf9ff10f59887640187fc2a3b08adacce053e6a
jeehyuni18 <jeehyuni18@gmail.com>
2021-07-17 00:51:44 +0900

    article entity 추가

---
+++ src/main/java/com/hh99/velog/dto/ArticleRequestDto.java
@@ -0,0 +1,13 @@
+package com.hh99.velog.dto;
+
+public class ArticleRequestDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private int commentCount;
+    private String username;
+    private int likeCount;
+    private String profileImage;
+    private String thumbnail;
+}
